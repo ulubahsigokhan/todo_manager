d6b210f466d63a4c265542a0bf1f4198b5c9e2ab
ulubahsigokhan <g.ulubahsi.tech@gmail.com>
2025-12-08 21:41:07 -0800

    Implement task list printing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 import model.Task;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Task t1 = new Task("Learn Java", Task.Priority.HIGH);
-        System.out.println(t1.getStatusMessage());
 
-        t1.setCompleted(true);
-        System.out.println(t1.getStatusMessage());
+        Task task = new Task("Learn Java", Task.Priority.HIGH);
+        Task t1 = new Task("Practice Git", Task.Priority.MEDIUM);
+        Task t2 = new Task("Test automation study", Task.Priority.LOW);
+
+        // NOW create the list
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        tasks.add(task);
+        tasks.add(t1);
+        tasks.add(t2);
+
+        System.out.println("Total tasks stored: " + tasks.size());
+
+        for (Task t : tasks) {
+            System.out.println("ID: " + t.getId() + " | Title: " + t.getStatusMessage());
+        }
     }
 }
